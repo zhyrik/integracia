41d7a6ed4af56580f74f2de44d6201edace1df47
zhyrik <zhyrki@ukr.net>
2019-12-28 15:55:45 +0200

    change home title

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import IndexForm from '../components/index/IndexForm'
 const IndexPage = () => (
   <Layout>
     <SEO
-      title="Агенція інтеграції"
+      title="Wroclaw"
       lang="uk"
     />
     <Section>
